266c661bffc7622a1b04027331cdb719a957e8d0
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-19 23:37:19 +0200

    Created weather Slice file

---
+++ src/feature/WeatherApp/weatherAppSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const weatherSlice = createSlice({
+    name: "weather",
+    initialState: {
+        weatherDetails: null,
+        status: "initial",
+    },
+    reducers: {
+        fetchWeather: () => ({
+            status: "loading",
+            weatherDetails: null,
+        }),
+        fetchWeatherSuccess: (_, {payload: weatherDetails }) => ({
+            status: "success",
+            weatherDetails,
+        }),
+        fetchWeatherError: () => ({
+            status: "error",
+            weatherDetails: null,
+        }),
+    },
+});
+
+export const {
+    fetchWeather,
+    fetchWeatherSuccess,
+    fetchWeatherError
+} = weatherSlice.actions;
+
+const selectWeatherState = state => state.weather
+
+export const selectWeatherDetails = state => selectWeatherState(state).weatherDetails
+export const selectWeatherStatus = state => selectWeatherState(state).status
+
+export default weatherSlice.reducer;
