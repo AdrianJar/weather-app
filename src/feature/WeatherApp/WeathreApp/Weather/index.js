7bde9f673f7421fad6507b72454b68c94543c4cb
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-19 23:38:08 +0200

    Created main weather component

---
+++ src/feature/WeatherApp/WeathreApp/Weather/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectWeatherDetails } from '../../weatherAppSlice';
+import { City, Icon, Temp, WeatherWrapper, Wrapper } from './styled';
+
+const Weather = () => {
+
+    const data = useSelector(selectWeatherDetails)
+    const windSpeed = data.wind.speed*3.6;
+
+    return (
+        <Wrapper>
+            <WeatherWrapper>
+                <Icon>Sun</Icon>
+                <div>
+                    <Temp>{data.main.temp.toFixed(1)}°C</Temp>
+                    <City>{data.name}</City>
+                </div>
+            </WeatherWrapper>
+
+
+            <h2>Feels like {data.main.feels_like.toFixed(1)}°C</h2>
+            <h2>{data.main.humidity}% humidity</h2>
+            <h2>{windSpeed.toFixed(1)}KM/H</h2>
+        </Wrapper>
+    )
+}
+
+export default Weather
