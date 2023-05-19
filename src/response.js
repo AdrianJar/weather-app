5b8ba06538a9ab6d8697b671b79c352368f9e5d8
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-19 23:36:23 +0200

    added response eg.

---
+++ src/response.js
@@ -0,0 +1,41 @@
+const response = {
+    "coord": {
+        "lon": -0.1257,
+        "lat": 51.5085
+    },
+    "weather": [{
+        "id": 803,
+        "main": "Clouds",
+        "description": "broken clouds",
+        "icon": "04d"
+    }],
+    "base": "stations",
+    "main": {
+        "temp": 289.1,
+        "feels_like": 288.42,
+        "temp_min": 287.35,
+        "temp_max": 290.01,
+        "pressure": 1026,
+        "humidity": 64
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 4.12,
+        "deg": 110
+    },
+    "clouds": {
+        "all": 75
+    },
+    "dt": 1684517706,
+    "sys": {
+        "type": 2,
+        "id": 2019646,
+        "country": "GB",
+        "sunrise": 1684469018,
+        "sunset": 1684525832
+    },
+    "timezone": 3600,
+    "id": 2643743,
+    "name": "London",
+    "cod": 200
+}
